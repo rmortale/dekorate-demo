b1ae3b04c37a05ffb0a31047dffa443600732a4d
Antonio <dolce.antonio@gmail.com>
2020-07-26 15:46:24 +0200

    added route

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.dekorate.openshift.annotation.OpenshiftApplication;
 
 @SpringBootApplication
-//@OpenshiftApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
