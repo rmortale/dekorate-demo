541d7e18b75f47a9d69dcd75d817a4e090b86823
Antonio Dolce <dolce.antonio@gmail.com>
2024-11-09 23:37:15 +0100

    initial commit

---
+++ src/main/java/ch/dulce/k8s/dekoratedemo/DekorateDemoApplication.java
@@ -0,0 +1,12 @@
+package ch.dulce.k8s.dekoratedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekorateDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DekorateDemoApplication.class, args);
+  }
+}
