a83fc47bc6e05204ad054525175c0d7e463885ad
Antonio Dolce <dolce.antonio@gmail.com>
2024-11-12 22:46:35 +0100

    try docker buildTagPublish Action

--- src/main/java/ch/dulce/k8s/dekoratedemo/DekorateDemoApplication.java
+++ src/main/java/ch/dulce/k8s/dekoratedemo/DekorateDemoApplication.java
@@ -1,9 +1,11 @@
 package ch.dulce.k8s.dekoratedemo;
 
+import io.dekorate.docker.annotation.DockerBuild;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@DockerBuild(group = "rmortale", registry = "ghcr.io")
 public class DekorateDemoApplication {
 
   public static void main(String[] args) {
