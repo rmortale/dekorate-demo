e7fb0442e7d45b089236bee071d028667626122b
Antonio Dolce <dolce.antonio@gmail.com>
2024-11-30 17:27:09 +0100

    extract tag

--- src/main/java/ch/dulce/k8s/dekoratedemo/controller/HelloController.java
+++ src/main/java/ch/dulce/k8s/dekoratedemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package ch.dulce.k8s.dekoratedemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/hello")
 public class HelloController {
 
-  @GetMapping
+  @Value("${app.greeting}")
+  private String greeting;
+
+  @GetMapping()
   public String hello() {
-    return "Hello World 6!";
+    return greeting;
   }
 }
